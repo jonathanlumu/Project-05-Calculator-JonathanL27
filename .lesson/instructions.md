8fec0d302f43bb2600f115799b5ad3b61bbcd931
jonathanlumu <135996867+jonathanlumu@users.noreply.github.com>
2023-08-21 08:25:39 +0000

    modification du code

--- .lesson/instructions.md
+++ .lesson/instructions.md
@@ -1,20 +1,27 @@
-# Instructions  
+ # Instructions
 
-  ** this file should contain student lesson instructions **
+## Screens
+* The bottom input (*can be selected with id value* `input`) holds the current input number.
+* The upper label (*can be selected with id value* `calcul`) holds the current computation string as the user types. Operators are preceeded and followed by a single space.
+* A number is added to the label after pressing an operator.
+* Le calcul ne se fait que quand on apuie sur `=`ou `%`. `
+* For example, the computation `23 × 2 = 46`, will have `2` in the input and the string `23 × 2` in the text above the input field.
 
-  _ students will see these instructions in a read-only workspace tab _
+## Buttons
+Buttons are the ***ONLY WAY*** to input data and work as they do in a physical calculator as follows:
+- Pressing the numpad adds the digit on the input value
+- The dot `.` can be in a string only once and can only follow a digit
+- Zero cannot be repeated at the beginning of a number eg. `0000` is not valid input value
+- Pressing the `+/-` button changes the sign of the current input number
+- Pressing the `%` button returns the calculations result as a percentage. i.e. divides it a 100.
+- `AC` resets everything, and `C` clears the input alone.
+- When displaying the result of an operation, an equal sign `=` is added to the input string and the results is diplayed in the input field.
 
-  ## Steps
-  1. 
-  2. 
-  3. 
-
-  Use [Markdown](https://gist.github.com/cuonggt/9b7d08a597b167299f0d) to format your instructions.
-
-  For example, here is a code block in python3
-```python
-def hello_world():
-  print("hello world!")
-```
-
-  
+## Bonus
+- Ajouter un bouton `00` qui ajoute deux zéros à l'entrée de l'utilisateur
+- Ajouter un historique de calcul.
+- Ajouter à l'historique la possibilité de recouvrir un calcul passé. Le calcul fait dans l'input secondaire, le resultat dans l'input primaire.
+- Enlever le curseur qui ***clignote*** dans l'input.
+- Ajouter une section des boutons pour les constantes `π`, `e`, etc.
+- Ajouter une section des boutons pour les fonctions: `exp`, `ln`, trigonometriques, etc.
+- Ajouter la possibilite de calcul recursif c-à-d reprendre la derniere operation ou le premier operand est la reponse du calcul precedent. Par exemple faire `3 x 2` nous donne `6` comme reponse. En appuyant de nouveau sur `=` on fait cette fois-ci `6 x 2` puis `12 x 2`, etc.
--- script.js
+++ script.js
@@ -1,58 +1,133 @@
-const form = document.querySelector("form");
-const buttons = form.querySelectorAll("button");
-const label = document.querySelector("#input");
-const userInput = form.elements["userInput"];
-
-import {
-  percentageSign,
-  equalsSign,
-  handleEqualsClick,
-  handlePercentageClick,
-  handleOtherOperatorsClick,
-  handleResetClick,
-  handleButtonClick,
-  clearResult,
-} from "./calculator.js";
-
-label.textContent = "";
-userInput.value = "";
-
-function handleSubmitClick(textContent) {
-  if (textContent === equalsSign) {
-    handleEqualsClick(label, userInput);
-  } else if (textContent === percentageSign) {
-    handlePercentageClick(userInput, label);
-  } else {
-    handleOtherOperatorsClick(textContent, userInput, label);
-  }
-}
-
-buttons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    switch (this.type) {
-      case "submit":
-        handleSubmitClick(this.textContent);
-        break;
-      case "reset":
-        handleResetClick(label, form);
-        break;
-      case "button":
-        handleButtonClick(this.textContent, userInput);
-        break;
-      default:
-        break;
-    }
+import {calculate} from "./calculator.js";
+// TODO: Faire la manipulation du DOM dans ce fichier
+const buttonCal = document.getElementById("calcul");
+const buttonInput = document.getElementById("input");
+const buttonAc = document.getElementById("reset");
+const buttonClear = document.getElementById("clear");
+const buttonPlusoumoins = document.getElementById("plusoumoins");
+const buttonPourcentage = document.getElementById("percentage");
+const buttonDiv = document.getElementById("divideby");
+const buttonChiffres = document.querySelectorAll(".numpad");
+const buttonMult = document.getElementById("times");
+const buttonSoustrac = document.getElementById("minus");
+const plusButton = document.getElementById("plus");
+const buttonEgal = document.getElementById("equals");
+plusButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  buttonCal.innerHTML = `${(buttonInput.value)}+`;
+  buttonInput.value = "";
+})
+buttonMult.addEventListener('click', (event) => {
+  event.preventDefault();
+  buttonCal.innerHTML = `${(buttonInput.value)}×`;
+  buttonInput.value = "";
+})
+buttonDiv.addEventListener('click', (event) => {
+  event.preventDefault();
+  buttonCal.innerHTML = `${(buttonInput.value)}÷`;
+  buttonInput.value = "";
+})
+buttonSoustrac.addEventListener('click', (event) => {
+  event.preventDefault();
+  buttonCal.innerHTML = `${(buttonInput.value)}-`;
+  buttonInput.value = "";
+})
+let currentValue;
+let previousValue;
+let operatorSign = '';
+let display = '';
+buttonChiffres.forEach((button) => {
+  button.addEventListener('click', () => {
+    buttonInput.value += button.textContent
   });
 });
-
-userInput.addEventListener("input", function() {
-  this.value = this.value.match(/[0-9.]*/)[0];
-});
-
-form.addEventListener("submit", function(event) {
+//  clear function
+function clear() {
+  buttonInput.value = '';
+  display = '';
+}
+buttonClear.addEventListener('click', clear);
+//  reset function
+function reset() {
+  location.reload;
+}
+buttonAc.addEventListener('click', reset);
+// function pour addition
+function addition(event) {
   event.preventDefault();
-});
-
-form.addEventListener("reset", function() {
-  clearResult();
-});
+  if (display) {
+    operatorSign = '+';
+    buttonCal.innerText = buttonCal.innerText + ' ' + buttonInput.value + ' +';
+    clear();
+  } else {
+    clear();
+  }
+}
+plusButton.addEventListener('click', addition);
+// function pour soustraction
+function soustraction(event) {
+  event.preventDefault();
+  if (display) {
+    operatorSign = '-';
+    buttonCal.innerText = buttonCal.innerText + ' ' + buttonInput.value + '-';
+    clear();
+  } else {
+    clear();
+  }
+}
+buttonSoustrac.addEventListener('click', soustraction);
+// function pour division
+function division(event) {
+  event.preventDefault();
+  if (display) {
+    operatorSign = '/';
+    buttonCal.innerText = buttonCal.innerText + ' ' + buttonInput.value + ' /';
+    clear();
+  } else {
+    clear();
+  }
+}
+buttonDiv.addEventListener('click', division);
+// function pour multiplication
+function multiplication(event) {
+  event.preventDefault();
+  if (display) {
+    operatorSign = '*';
+    buttonCal.innerText = buttonCal.innerText + ' ' + buttonInput.value + ' *';
+    clear();
+  } else {
+    clear();
+  }
+}
+buttonMult.addEventListener('click', multiplication);
+// function pour plusoumoins
+function plusoumoins(event) {
+  event.preventDefault();
+  if (display) {
+    if (buttonInput.value.includes('-')) {
+    }
+    buttonInput.value = display;
+  } else {
+    buttonInput.value = '.' + display;
+  }
+}
+buttonPlusoumoins.addEventListener('click', plusoumoins);
+// function pourcentage
+function pourcentage(event) {
+  event.preventDefault();
+  // display.innerText
+  HTMLButtonElement.pourcentage = buttonInput.value + '%';
+  buttonInput.value = parseInt(buttonInput.value) / 100;
+}
+buttonPourcentage.addEventListener('click', pourcentage);
+// function egal
+function egal(event) {
+  event.preventDefault();
+  if (buttonCal.innerText.includes('=')) {
+  } else {
+    buttonCal.innerText = buttonCal.innerText + ' ' + buttonInput.value;
+    buttonInput.value = eval(buttonCal.innerText);
+    buttonCal = buttonCal.innerText + ' ' + '=';
+  }
+}
+buttonEgal.addEventListener('click', egal);
